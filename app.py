8fdda1ba05a463564e555cc7d211364402e9181b
avrohom-schneierson <101535424+avrohom-schneierson@users.noreply.github.com>
2023-06-06 22:39:11 -0400

    Added server error handling

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request
 import json
+from werkzeug.exceptions import HTTPException
+
 import tools
 import db
 
-### The server app ###
+##########################
+#     The server app     #
+##########################
 
 app = Flask(__name__)
-
-# Flag indicating whether server should utilize the db by first attempting to retrieve results from it, if false,
-# then it will always use the API.
-USE_DB = True
+USE_DB = True  # If false, then it will always use the API
 
 
 @app.route('/')
@@ -17,13 +18,13 @@ USE_DB = True
 @app.route('/index')
 @app.route('/search')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 
-# Return the available languages for news searches. No args expected.
+# Used for news searches. No args expected.
 @app.route('/internal/get-languages')
 def get_languages():
-    result = db.retrieve_one('enums', {'name': 'language'}, {'_id': 0, 'values': 1})
+    result = db.news_db.get_collection('enums').find_one({'name': 'language'}, {'_id': 0, 'values': 1})
     if result is not None:
         languages = result.get('values', {})
         return_dict = tools.get_template_response_dict(
@@ -36,10 +37,10 @@ def get_languages():
     return json.dumps(return_dict)
 
 
-# Return the available countries for news searches. No args expected.
+# Used for news searches. No args expected.
 @app.route('/internal/get-countries')
 def get_countries():
-    result = db.retrieve_one('enums', {'name': 'country'}, {'_id': 0, 'values': 1})
+    result = db.news_db.get_collection('enums').find_one({'name': 'country'}, {'_id': 0, 'values': 1})
     if result is not None:
         countries = result.get('values')
         return_dict = tools.get_template_response_dict(
@@ -52,10 +53,10 @@ def get_countries():
     return json.dumps(return_dict)
 
 
-# Return the available categories for news searches. No args expected.
+# Used for news searches. No args expected.
 @app.route('/internal/get-categories')
 def get_categories():
-    result = db.retrieve_one('enums', {'name': 'category'}, {'_id': 0, 'values': 1})
+    result = db.news_db.get_collection('enums').find_one({'name': 'category'}, {'_id': 0, 'values': 1})
     if result is not None:
         categories = result.get('values')
         return_dict = tools.get_template_response_dict(
@@ -77,30 +78,24 @@ def get_categories():
 #   'country'
 #   'category'
 #   'searchIn'
-# For the dates ('from' and 'to' args), make the request to this method with either a utc date, or a local date denoted with the time offset.
-# The function will then return the results for the corresponding UTC time.
+# For the dates ('from' and 'to' args), make the request to this method with either an utc date, or a local date denoted with the time offset.
+# The function will then return the results for the corresponding UTC time range.
 @app.route('/internal/get-articles')
 def get_articles():
     succeeded = False
     errors = {}
-
-    # Get a return_dict with our url for this endpoint, and the args.
     return_dict = tools.get_template_response_dict(
         request.base_url, request.args)
     if len(request.args.get('q', '')) > 0:
-        num_articles_to_return = 10  # These will be displayed as the most recent articles on the home page
-
-        # Retrieve the news search (either via DB if it is already there, else via API)
+        num_articles_to_return = 10
         succeeded, result, errors = tools.retrieve_news_search(
             USE_DB, request.args)
 
-        # If successfully retrieved then pack the results
         if succeeded:
             return_dict['succeeded'] = True
             return_dict['results']['num_results'] = 2
             return_dict['results']['values'] = {
                 'num_articles': result.get('totalResults'), 'articles': result.get('articles')[:num_articles_to_return]}
-    # Otherwise, pack error result
     if not succeeded:
         return_dict['errors']['error_source'] = errors.get('error_source', 'internal')
         return_dict['errors']['message'] = errors.get('message', 'invalid parameters provided: missing parameter \'q\'')
@@ -109,7 +104,7 @@ def get_articles():
 
 
 # Return the total number of times the term appears on all webpages for a news search.
-# Args expected: the args used for the news search.
+# Expects the args used for the news search.
 # The function will look up the list of article urls in the db for the given search parameters, and return the sum of
 # their term counts for the term.
 @app.route('/internal/get-num-term-occurrences')
@@ -121,7 +116,6 @@ def get_num_term_occurrences():
     results = db.retrieve_news_search(args)
     if results is not None and 'articles' in results:
         urls = [article.get('url') for article in results.get('articles')]
-        # Get the num of occurrences on each page
         num_occurrences = tools.num_occurrences_on_pages(USE_DB, args.get('q'), urls)
         return_dict = tools.get_template_response_dict(
             request.base_url, request.args, num_results=1, succeeded=True, errors={},
@@ -131,3 +125,11 @@ def get_num_term_occurrences():
             request.base_url, request.args, num_results=0, succeeded=False,
             errors={'error_source': 'internal', 'message': 'error retrieving article urls'})
     return json.dumps(return_dict)
+
+
+# Source: https://github.com/BeeFriedman/FluxNodeUptimeMonitor/blob/da0f425b5bf64efa62e4d7c05ec1b16df0282dc5/app.py#LL40C6-L40C6
+@app.errorhandler(Exception)
+def handle_exception(e):
+    if isinstance(e, HTTPException):
+        return str(e.code) + ' error', e.code
+    return '500 internal server error', 500
