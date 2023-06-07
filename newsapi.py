070dcccc6a2f21ddcfca1adf7a8c961a6bf0870f
avrohom-schneierson <101535424+avrohom-schneierson@users.noreply.github.com>
2023-06-06 22:41:17 -0400

    Add files via upload

--- newsapi.py
+++ newsapi.py
@@ -1,11 +1,12 @@
 import json
 import requests
 from requests import Response
-import logging
 
-################# This module defines functions for using the news api (newsapi.org) #################
+##############################################################################
+#     This module defines functions for using the news api (newsapi.org)     #
+##############################################################################
 
-USE_API = True
+USE_API = False
 BASE_URL = 'https://newsapi.org'
 api_key_file = open(r"tokens/newsapiorg_apikey.txt", "r")
 API_KEY = api_key_file.readline()
@@ -14,8 +15,10 @@ headers = {
     'Authorization': API_KEY if USE_API else ""
 }
 
+################################################
+#     Simple wrappers of newsapi.org's API     #
+################################################
 
-######### Simple wrappers of newsapi.org's API #########
 
 # Get all news sources. All parameters are optional and can be used as filters, but not required.
 def get_sources(**params) -> Response:
@@ -33,30 +36,28 @@ def get_articles(params) -> Response:
     return result
 
 
-######### Helper newsapi functions #########
+################################################
+#     Helper newsapi functions     #
+################################################
 
-# Return articles for the given arguments from ALL pages. Does not take a page argument, that will be handled internally here.
+# Does not take a page argument, that will be handled internally here.
 def get_articles_all_pages(**params) -> Response | None:
     # Copy arguments and add a page arg
     arguments = dict(params)
     arguments['page'] = 1
-
     articles = []
 
     # While we successfully retrieved additional articles and did not yet retrieve all of them: collect the articles and request the next page
     while True:
-        # Get articles from the next page
         response = get_articles(arguments)
         response_content = json.loads(response.content)
         articles += response_content.get('articles', [])
         arguments['page'] += 1
 
-        # If the latest request failed, we finished retrieving all articles, or the response for some reason doesn't contain the
-        # 'totalResults' value - then break out of the loop.
+        # If the request failed or we are done collecting all articles, then break out of the loop
         if response.status_code != 200 or len(articles) >= response_content.get('totalResults', float('inf')):
             break
 
-    # Update the response object's content with all the articles we've received. If we hit an error, do not return the result as it is incomplete.
     if response.status_code == 200:
         response_content.update({'articles': articles})
         response._content = json.dumps(response_content)
